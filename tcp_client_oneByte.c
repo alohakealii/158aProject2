99677b1a6b0abb57fbbbd0eade1603e9149c67b1
alexandralomotan <alexandralomotan@gmail.com>
2015-03-31 14:51:29 -0700

    Now does 1000 loop

--- tcp_client_oneByte.c
+++ tcp_client_oneByte.c
@@ -5,7 +5,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+#include <sys/time.h>
 
 void error(const char *msg)
 {
@@ -28,27 +29,66 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    
+    //start timer
+    struct timeval startTime, finishTime;
+    gettimeofday(&startTime, NULL);
+    
+    //loop 1000 times
+    int i;
+    for (i = 0; i < 1000; i++) {
+        n = write(sockfd,buffer,strlen(buffer));
+        if (n < 0) 
+            error("ERROR writing to socket");
+        bzero(buffer,256);
+        n = read(sockfd,buffer,255);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        printf("%s\n",buffer);
+    }
+    
+    // finish time
+    gettimeofday(&finishTime, NULL);
+    printf("Start time: %lu.%d\nFinish time: %lu.%d\n", startTime.tv_sec, startTime.tv_usec, finishTime.tv_sec, finishTime.tv_usec);
+    unsigned long totalSec = finishTime.tv_sec - startTime.tv_sec;
+    unsigned long usec;
+    if (finishTime.tv_usec > startTime.tv_usec) {
+        usec = finishTime.tv_usec - startTime.tv_usec;
+    }
+    else {
+        usec = 1000000 - startTime.tv_usec + finishTime.tv_usec;
+    }
+    unsigned long total = totalSec * 1000000 + usec;
+    printf("Total latency: %lu\nAverage latency: %lu\n", total, total/1000);
+    
     close(sockfd);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
