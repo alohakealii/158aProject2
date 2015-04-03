71340d31254e03d9d1bcea02104a3b4865cd987d
David Delgado <alohakealii@gmail.com>
2015-04-03 14:13:42 -0700

    KB update

--- tcp_client_KB.c
+++ tcp_client_KB.c
@@ -13,6 +13,19 @@ void error(const char *msg)
     exit(0);
 }
 
+void rcvTCP(int sockfd, char *buffer, char *message, int size, int i) {
+    memset(message, 0, sizeof(message));
+    int n;
+    while (strlen(message) < size) {
+        memset(buffer, 0, sizeof(buffer));
+        n = read(sockfd, buffer, sizeof(buffer));
+        if (n < 0) 
+             error("ERROR reading from socket");
+        strcat(message, buffer);
+    }
+    printf("R: %cx%d %d\n",message[0], strlen(message), i);
+}
+
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
@@ -20,11 +33,15 @@ int main(int argc, char *argv[])
     struct hostent *server;
 
     char buffer[64000];
-    memset(buffer,0,64000);
+    char message[64000];
+    memset(buffer, 0, sizeof(buffer));
+    memset(message, 0, sizeof(message));
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
+    // setup socket
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -33,84 +50,63 @@ int main(int argc, char *argv[])
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+
+    // connect
+    if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
+
     int i;
-    for (i = 0; i < 100; i++) {
+    for (i = 1; i < 101; i++) {
         memset(buffer,'A',1024);
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("ERROR writing to socket");
+            error("ERROR writing to socket");
         printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
-        memset(buffer,0,1024);
-        n = read(sockfd,buffer,1024);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("R: %cx%d %d\n",buffer[0], strlen(buffer), i);
+        rcvTCP(sockfd, buffer, message, 1024, i);
     }
-    for (i = 0; i < 100; i++) {
+    for (i = 1; i < 101; i++) {
         memset(buffer,'B',4096);
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("ERROR writing to socket");
+            error("ERROR writing to socket");
         printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
-        memset(buffer,0,4096);
-        n = read(sockfd,buffer,4096);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("R: %cx%d %d\n",buffer[0], strlen(buffer), i);
+        rcvTCP(sockfd, buffer, message, 4096, i);
+
     }
-    for (i = 0; i < 100; i++) {
+    for (i = 1; i < 101; i++) {
         memset(buffer,'C',8192);
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("ERROR writing to socket");
+            error("ERROR writing to socket");
         printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
-        memset(buffer,0,8192);
-        n = read(sockfd,buffer,8192);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("R: %cx%d %d\n",buffer[0], strlen(buffer), i);
+        rcvTCP(sockfd, buffer, message, 8192, i);
     }
-    for (i = 0; i < 100; i++) {
+    for (i = 1; i < 101; i++) {
         memset(buffer,'D',16384);
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("ERROR writing to socket");
-         printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
-        memset(buffer,0,16384);
-        n = read(sockfd,buffer,16384);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("R: %cx%d %d\n",buffer[0], strlen(buffer), i);
+            error("ERROR writing to socket");
+        printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
+        rcvTCP(sockfd, buffer, message, 16384, i);
     }
-    for (i = 0; i < 100; i++) {
+    for (i = 1; i < 101; i++) {
         memset(buffer,'E',32768);
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("ERROR writing to socket");
-         printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
-        memset(buffer,0,32768);
-        n = read(sockfd,buffer,32768);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("R: %cx%d %d\n",buffer[0], strlen(buffer), i);
+            error("ERROR writing to socket");
+        printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
+        rcvTCP(sockfd, buffer, message, 32768, i);
     }
-    for (i = 0; i < 100; i++) {
+    for (i = 1; i < 101; i++) {
         memset(buffer, 'F', sizeof(buffer));
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("ERROR writing to socket");
+            error("ERROR writing to socket");
         printf("S: %cx%d %d   ",buffer[0], strlen(buffer), i);
-        memset(buffer, 0, sizeof(buffer));
-        n = read(sockfd,buffer, sizeof(buffer));
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("R: %cx%d %d\n", buffer[0], strlen(buffer), i);
+        rcvTCP(sockfd, buffer, message, 64000, i);
     }
     memset(buffer, 0, sizeof(buffer));
     sprintf(buffer, "done");
--- tcp_server_KB.c
+++ tcp_server_KB.c
@@ -1,5 +1,3 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,44 +12,92 @@ void error(const char *msg)
     exit(1);
 }
 
+void rcvTCP(int sockfd, char *buffer, char *message, int size, int i) {
+    memset(message, 0, sizeof(message));
+    int n;
+    while (strlen(message) < size) {
+        memset(buffer, 0, sizeof(buffer));
+        n = read(sockfd,buffer,1024);
+        if (n < 0) 
+             error("ERROR reading from socket");
+        strcat(message, buffer);
+        printf("strlen(message)=%d  size=%d\n", strlen(message), size);
+    }
+    printf("R: %cx%d %d   ",message[0], strlen(message), i);
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[64000];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[64000];
+    char message[64000];
+    struct sockaddr_in serv_addr, cli_addr;
+    clilen = sizeof(cli_addr);
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    // setup socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
         error("ERROR opening socket");
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    // bind
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
         error("ERROR on binding");
-     listen(sockfd,500);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     n = 101;
-     int count = 0;
-     while (n > 100) {
-         memset(buffer, 0, sizeof(buffer));
-         n = read(newsockfd, buffer, sizeof(buffer));
-         if (n < 0) error("ERROR reading from socket");
-         printf("R: %cx%d %d    ",buffer[0], strlen(buffer), count);
-         n = write(newsockfd,&buffer,strlen(buffer));
-         if (n < 0) error("ERROR writing to socket");
-         printf("S: %cx%d\n",buffer[0], strlen(buffer));
-         count++;
-     }
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+
+    // listen
+    listen(sockfd,10);
+
+    // accept
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) 
+        error("ERROR on accept");
+
+    int i;
+    int n;
+    int size = 0;
+    for (i = 0; i < 6; i++) {
+        memset(message, 0, sizeof(message));
+        memset(buffer, 0, sizeof(buffer));
+        if (i == 0)
+            size = 1 * 1024;
+        else if (i == 1)
+            size = 4 * 1024;
+        else if (i == 2)
+            size = 8 * 1024;
+        else if (i == 3)
+            size = 16 * 1024;
+        else if (i == 4)
+            size = 32 * 1024;
+        else
+            size = 64000;
+        int j;
+        for (j = 1; j < 101; j++) {
+            rcvTCP(newsockfd, buffer, message, size, j);
+            n = write(newsockfd, message, strlen(message));
+            if (n < 0)
+                error("Error writing to socket");
+            printf("S: %cx%d %d\n", message[0], strlen(message), j);
+        }      
+    }
+
+    memset(buffer, 0, sizeof(buffer));
+    n = read(newsockfd, buffer, sizeof(buffer));
+    if (n < 0)
+        error("Error reading from socket");
+    printf("Last receive: %s\n", buffer);
+    n = write(newsockfd, buffer, strlen(buffer));
+    if (n < 0)
+        error("Error writing to socket");
+
+    close(newsockfd);
+    close(sockfd);
+    return 0; 
 }
